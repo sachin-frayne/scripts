0fd75869f6844966f5a38d3b8a2d0fa0ad0f3fd2
sachin-frayne <sachin.frayne@gmail.com>
2023-03-07 10:59:26 +0100

    add a help, list and specific software option

--- mac.sh
+++ mac.sh
@@ -2,6 +2,44 @@
 
 set -euo pipefail
 
+function help {
+  echo "
+SYNOPSIS
+       ./mac [-b BINARY]
+
+OPTIONS
+
+       -h        Help
+       -b        The binary of the software/settings to install for example, alfred
+       -l        List the software/settings that can be installed
+"
+}
+
+BINARY=""
+LIST=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -b|--binary)
+      BINARY="${2}"
+      shift
+      shift
+      ;;
+    -l|--list)
+      LIST=true
+      shift
+      ;;
+    -h|--help)
+      help
+      exit 0
+      ;;
+    -*|--*)
+      echo "Unknown option ${1}"
+      exit 1
+      ;;
+  esac
+done
+
 LIB=$(echo ~/Documents/scripts/bin/mac)
 
 brew_install () {
@@ -11,6 +49,22 @@ brew_install () {
 brew_install_cask () {
     if brew ls --versions --cask "$1" > /dev/null; then brew upgrade --cask "$1" 2> /dev/null; else brew install --cask "$1"; fi
 }
+
+if [[ ${LIST} == true ]]
+then
+  for F in $LIB/*
+  do
+    echo "$(echo $F | sed 's|'$LIB'/||g' | sed 's|\.sh||g')"
+  done
+  exit
+fi
+
+if [[ ! -z ${BINARY} ]]
+then
+  echo "Check for $(echo $BINARY | sed 's|'$LIB'/||g' | sed 's|\.sh||g')" 
+  source $LIB/${BINARY}.sh
+  exit
+fi
 
 for F in $LIB/*
 do
